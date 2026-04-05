d5a858d968da64cdd527ced9017e4ca8697fe4e8
Nong Duy Khanh <ngdykhnh@gmail.com>
2026-04-05 16:44:04 +0700

    refactor

--- src/features/class/api/classAppApi.ts
+++ src/features/class/api/classAppApi.ts
@@ -2,6 +2,6 @@ import axiosClient from "../../../shared/api/axiosClient";
 import { ClassApplicationDTO } from "../model/classTypes";
 
 export const getMyClassApplications = async (): Promise<ClassApplicationDTO[]> => {
-    const res = await axiosClient.get("/class-applications");
+    const res = await axiosClient.get("/tutor/class-applications");
     return res.data.items ?? [];
 };
