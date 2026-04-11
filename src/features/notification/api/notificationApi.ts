ed3ed0fdf63615620056f20094f3344eb4479e57
nviquang <nguyenquang5010@gmail.com>
2026-04-11 22:37:26 +0700

    Revert "cập nhật thông báo"
    This reverts commit 16ddc526d21de711f115c03464d848dcd2e957d7.

--- src/features/notification/api/notificationApi.ts
+++ src/features/notification/api/notificationApi.ts
@@ -1,11 +1,6 @@
 import axiosClient from "../../../shared/api/axiosClient";
-import type { NotificationType } from "../model/notificationType";
 
 export const notificationApi = {
-    async getMine() {
-        const { data } = await axiosClient.get<NotificationType[]>("/notifications/me");
-        return data;
-    },
     async hasNew() {
         const { data } = await axiosClient.get<{ hasNew: boolean }>("/notifications/has-new");
         return data.hasNew;
